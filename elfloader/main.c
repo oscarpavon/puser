0e07c1d91e5e36c83715afd37086a8cd1cce34b7
Oscar Pavon <pavon_oscar@outlook.com>
2024-12-07 06:41:10 +0000

    ready to call the program

--- elfloader/main.c
+++ elfloader/main.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include "elf.h"
 
+#define ELFABI __attribute__((sysv_abi))
+
 static void elf_get_image_size(
 	struct ElfHeader* kernel_header,
 	struct ElfProgramHeader* program_headers,
@@ -85,7 +87,55 @@ int main() {
 	image_size = image_end - image_begin;
 	printf("Image size: %ld\n",image_size);
 
+	uint8_t* image_address_pointer = malloc(image_size/page_size);
 
+	printf("Image addres %p\n",image_address_pointer);
+	
+	image_address = (uint64_t)image_address_pointer;
+
+	printf("Image address in uint64 %lx\n",image_address);
+
+	printf("Image begin %lx\n",image_begin);
+
+	for(int i ; i < 2;i++){
+		struct ElfProgramHeader* program_header = (struct ElfProgramHeader*)&program_headers[i];
+		printf("Program header virtual_address %lx\n",program_header->virtual_address);
+
+		uint64_t program_header_address;
+
+		program_header_address = image_address + 
+			program_header->virtual_address - image_begin;
+
+		uint8_t* in_bytes = (uint8_t*)program_header;
+		uint8_t* offset = in_bytes + program_header->offset;
+		printf("offset %lx\n",(uint64_t)offset);
+	
+		printf("program address %p\n",(uint64_t*)program_header_address);
+		printf("program address %lx\n",program_header_address);
+		char test[254];
+		memcpy((uint8_t*)program_header_address,(uint8_t*)offset,program_header->file_size);
+
+	}
+
+	uint64_t program_entry = image_address + header->entry - image_begin;
+	uint64_t* program_entry_address = (uint64_t*)program_entry;
+
+	printf("Program entry number %ld\n",program_entry);
+	printf("Program entry address %p\n",program_entry_address);
+
+	printf("now we can call the function\n");
+
+	void* start = (void*)program_entry_address;
+ ((void (*)())start)();
+	
+	//void(ELFABI*start)();
+	//start = (void(ELFABI*)())program_entry_address;
+
+	//(*start)();
+		
+
+
+	free(image_address_pointer);
 	free(binary);
 	fclose(file);
 
