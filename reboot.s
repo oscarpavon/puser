c268da4dca7d87f962242d0e497f6d4c23014982
Oscar Pavon <pavon_oscar@outlook.com>
2024-12-07 00:43:00 +0000

    added reboot

--- reboot.s
+++ reboot.s
@@ -6,7 +6,9 @@
 ; rsi	; 2nd param
 ; rdi	; 1st param
 
+SYS_SYNC = 162
 SYS_REBOOT = 169
+
 LINUX_REBOOT_MAGIC1 = 0xfee1dead
 LINUX_REBOOT_MAGIC2 = 0x28121969
 LINUX_REBOOT_CMD_RESTART = 0x1234567
@@ -16,12 +18,14 @@ format ELF64 executable 3
 segment readable executable
 
 entry $
+	mov rax,SYS_SYNC
+	syscall
 
-      mov rax,SYS_REBOOT
-			mov rdi,LINUX_REBOOT_MAGIC1
-			mov rsi,LINUX_REBOOT_MAGIC2
-			mov rdx,LINUX_REBOOT_CMD_RESTART
-      syscall
+	mov rax,SYS_REBOOT
+	mov rdi,LINUX_REBOOT_MAGIC1
+	mov rsi,LINUX_REBOOT_MAGIC2
+	mov rdx,LINUX_REBOOT_CMD_RESTART
+	syscall
 
 
 
